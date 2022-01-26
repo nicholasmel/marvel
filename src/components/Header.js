f1a289baaada63c79707e3603b30ebce7359009a
Nico Mel <nmel1@asu.edu>
2022-01-26 14:23:27 -0700

    update header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import styled from 'styled-components';
+import spiderman from '../images/spideyman.png'
 
 function Header() {
     return (
         <Container>
             <Quote>
-                <p>"With great power comes great responsibility."</p>
+                <h1>CREATE AWESOME STUFF WITH THE WORLD'S GREATEST COMIC API</h1>
+                <p>“With Great Power, There Must Also Come...Great Responsibility”</p>
+            </Quote>
+            <Quote>
+                <h1>Access information about Marvel's vast library of comics</h1>
+                <p>“Your Friendly Neighborhood Spider-Man”</p>
             </Quote>
         </Container>
     )
@@ -14,25 +20,35 @@ function Header() {
 export default Header
 
 const Container = styled.div`
-    background-image: url('https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/a58a7719-0dcf-4e0b-b7bb-d2b725dbbb8e/detfa8u-f8a9feb9-93bf-4ae8-b7e0-4d72d20ea73f.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2E1OGE3NzE5LTBkY2YtNGUwYi1iN2JiLWQyYjcyNWRiYmI4ZVwvZGV0ZmE4dS1mOGE5ZmViOS05M2JmLTRhZTgtYjdlMC00ZDcyZDIwZWE3M2YucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.-BT6dJjWBUWuv4jgnO3V9KepFENryC-0uDA3yYNRs_w');
-    background-position: top;
+    background-image: url(${spiderman});
+    background-position: center;
     background-repeat: no-repeat;
-    background-size: cover;
+    background-size: contain;
+    background-color: #911F30;
     overflow: hidden;
     height: 60vh;
     width: 100vw;
+    display: flex;
+    justify-content: center;
 `
 const Quote = styled.div`
     height: 20vh;
-    width: 20vw;
-    color: white;
-    background: 0000000;
-    margin: 120px 400px;
+    width: 24vw;
     justify-content: center;
+    justify-items: center;
     text-align: center;
+    padding: 10vw 14vw;
+
+    h1 {
+        padding-top: 20px;
+        font-size: large;
+        color: #e6e6e6;
+        text-transform: uppercase;
+    }
 
     p {
-        padding-top: 100px;
+        padding-top: 40px;
         font-style: italic;
+        color: #d6d6d6;
     }
 `
